3ab5048669d72dece3453bac26c4ba1c30a3e74e
Yohanes <yohanesmulugeta21@gmail.com>
2023-11-20 04:21:30 +0300

    make the pizza component work with the whole pizza dadta.

---
+++ src/components/Pizza.js
@@ -0,0 +1,5 @@
+function Pizza({ pizza }) {
+  return <h2>{pizza.name}</h2>;
+}
+
+export default Pizza;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import pizzaData from './data';
+import Pizza from './components/Pizza';
+
 function App() {
-  return <h1>Hello React!</h1>;
+  const pizzas = pizzaData.map((pizza) => <Pizza pizza={pizza} key={pizza.name} />);
+
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      {pizzas}
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
