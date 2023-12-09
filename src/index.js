ab024336208af70ad7cc319e9f6d2e9e762e0b32
Yohanes <yohanesmulugeta21@gmail.com>
2023-12-09 22:56:40 +0300

    implement rating compontents.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
+import StarRating from './components/StarRating';
 
-import App from './App';
+// import App from './App';
 
-import './index.css';
+// import './index.css';
 
 const root = createRoot(document.getElementById('root'));
 
-root.render(<App />);
+function Test() {
+  const [rating, setRating] = useState(0);
+
+  console.log(rating);
+
+  return (
+    <StarRating
+      onSet={setRating}
+      maxRating={5}
+      messages={['Terrible', 'Bad', 'Okay', 'Good', 'Excellent']}
+    />
+  );
+}
+
+root.render(
+  <React.StrictMode>
+    <Test />
+    <StarRating
+      maxRating={5}
+      size={20}
+      className="test"
+      color="tomato"
+      defaultRating={3}
+    />
+  </React.StrictMode>
+);
+// root.render(<App />);
